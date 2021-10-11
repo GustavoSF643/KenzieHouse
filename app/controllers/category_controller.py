2fb8c18bf84039009d05d454dea379517c1161a8
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-10-11 10:33:49 -0300

    feat: categories_route created

---
+++ app/controllers/category_controller.py
@@ -0,0 +1,19 @@
+
+def create_category():
+    ...
+
+
+def get_categories():
+    ...
+
+
+def get_category_by_id(id: int):
+    ...
+
+
+def update_category_by_id(id: int):
+    ...
+
+
+def delete_category_by_id(id: int):
+    ...
