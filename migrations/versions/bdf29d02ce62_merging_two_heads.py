963cec717a477c9f4be558107c98f75fe4eec41d
Wander Moreira <wandersantosmoreira@gmail.com>
2021-10-11 21:38:19 -0300

    fix: migration recovery

---
+++ migrations/versions/bdf29d02ce62_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: bdf29d02ce62
+Revises: 0fc87e00960a, 697c91194c86
+Create Date: 2021-10-11 21:17:25.960514
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bdf29d02ce62'
+down_revision = ('0fc87e00960a', '697c91194c86')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
