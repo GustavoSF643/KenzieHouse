a3d981582de162e3b3db97be7203a2b627db7216
Wander Moreira <wandersantosmoreira@gmail.com>
2021-10-18 10:40:08 -0300

    fix:admin authorization made through the db

--- app/services/admin_verify.py
+++ app/services/admin_verify.py
@@ -1,16 +1,10 @@
-from json import load
 from flask_jwt_extended import get_current_user
 
 def admin_verify(func):
 
     def wrapper(*args, **kwargs):
-
-        with open("admins.json", "r") as json_file:
-            admins = load(json_file)
-            admins_email = [admin['email'] for admin in admins]
-
-            if not get_current_user().email in admins_email:
-                return {"error":"Access restricted to authorized users only"}, 403
+        if not get_current_user().admin:
+            return {"error":"Access restricted to authorized users only"}, 403
 
         return func(*args, **kwargs)
 
