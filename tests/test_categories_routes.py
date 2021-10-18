0430b6963f44ce17c4188f4323cec3b3548312c0
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-10-18 12:55:04 -0300

    feat: tests for post status createds

--- tests/test_categories_routes.py
+++ tests/test_categories_routes.py
@@ -35,6 +35,56 @@ def test_if_POST_request_in_categories_route_return_status_code_CREATED():
 
     assert response.status_code == 201, "Incorrect status."
 
+def test_if_POST_request_with_a_invalid_json_in_categories_route_return_status_code_NOTACCEPTABLE():
+    invalid_dicts = [
+        {
+            "abc": "a",
+            "cba": "b"
+        },
+        {
+            "name": 1,
+            "description": 2
+        },
+        {
+            "name": 1.0,
+            "description": 2.0
+        }
+    ]
+
+    status_code_list = []
+    for invalid_dict in invalid_dicts:
+        response = test_client.post(
+            '/categories',
+            data=json.dumps(invalid_dict),
+            content_type='application/json'
+        )
+
+        status_code_list.append(response.status_code)    
+
+    for status_code in status_code_list:
+        assert status_code == 406, "Incorrect status."
+
+def test_if_POST_request_with_a_existent_category_in_categories_route_return_status_code_CONFLICT():
+    post_dict = {
+        "name": secrets.token_hex(10),
+        "description": secrets.token_hex(16)
+    }
+    
+    post_response = test_client.post(
+        '/categories',
+        data=json.dumps(post_dict),
+        content_type='application/json'
+    )
+
+    response = test_client.post(
+        '/categories',
+        data=json.dumps(post_dict),
+        content_type='application/json'
+    )
+    response_data = json.loads(post_response.get_data())
+    test_client.delete(f"/categories/{response_data['category_id']}")
+
+    assert response.status_code == 409, "Incorrect status."
 
 # Categories GET by id
 def test_categories_route_accepts_GET_request_by_id():
@@ -127,8 +177,8 @@ def test_if_PATCH_request_by_id_with_a_invalid_json_in_categories_route_return_s
 
     test_client.delete(f"/categories/{post_data['category_id']}")
 
-    for response in status_code_list:
-        assert response == 406, "Incorrect status."
+    for status_code in status_code_list:
+        assert status_code == 406, "Incorrect status."
 
 
 # Categories DELETE by id 
